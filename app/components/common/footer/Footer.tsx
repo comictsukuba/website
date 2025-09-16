b548ac85195c3666ac5e42f0b281c262b4b3b00b
Zemelua <red0165333@gmail.com>
2025-09-16 18:22:22 +0900

    style: フッターを改修

--- app/components/common/footer/Footer.tsx
+++ app/components/common/footer/Footer.tsx
@@ -1,25 +1,61 @@
+import { MailIcon, TwitterIcon } from "lucide-react";
 import type { JSX } from "react";
-import LinkButton from "~/components/common/button/LinkButton";
+import { Link } from "react-router";
 
 export function Footer(): JSX.Element {
     return (
-        <footer className="bg-[#00C1D4] font-nav text-white text-xs py-6 md:px-40 flex items-center justify-between shadow-md pl-10 md:pl-40 space-y-5">
-            <div className="space-y-5">
-                <div>
-                    <p className="text-[12px] font-bold">
+        <footer className="w-full h-fit bg-[#469fa3] text-white py-8 px-8 flex flex-col gap-8 items-center">
+            <div className="w-[1024px] max-w-full mx-auto flex flex-col gap-x-16 gap-y-8">
+                <div className="w-fit flex flex-col gap-2">
+                    <p className="font-bold text-xl">
                         コミックつくば！準備会
                     </p>
-                    <p> mail: comictsukubastaff@gmail.com</p>
+                    <ul className="grid grid-cols-[auto_1fr] gap-x-2 gap-y-1 text-sm">
+                        <li className="contents">
+                            <MailIcon size={20} />
+                            <span>comictsukubastaff@gmail.com</span>
+                        </li>
+                        <li className="contents">
+                            <TwitterIcon size={20} />
+                            <Link to="https://x.com/comictsukuba">@comictsukuba</Link>
+                        </li>
+                    </ul>
                 </div>
-                <div>
-                    <p>Copyright &copy; 2025 <strong>コミックつくば！準備会</strong> All Rights Reserved.</p>
+                <div className="w-full h-[1px] bg-white self-center" />
+                <div className="flex flex-row flex-wrap gap-x-8 gap-y-4">
+                    <NavGroup title="◇" links={[
+                        { label: "ホーム", href: "/" },
+                        { label: "コミックつくば！とは？", href: "/about" },
+                        { label: "お問い合わせ", href: "/contact" },
+                    ]} />
+                    <NavGroup title="一般参加" links={[
+                        { label: "開催情報", href: "#" },
+                        { label: "カタログ", href: "#" },
+                    ]} />
+                    <NavGroup title="サークル参加" links={[
+                        { label: "出展案内", href: "#" },
+                        { label: "サークル参加申込", href: "#" },
+                    ]} />
                 </div>
             </div>
-
-            <div className="mt-20 flex space-x-10 md:mr-30 mr-10 ml-20">
-                <LinkButton platform="twitter" url="https://x.com/comictsukuba" size={36} color="#FFFFFF" />
-                <LinkButton platform="instagram" url="https://instagram.com" size={36} color="#FFFFFF" />
+            <div>
+                <p>Copyright &copy; 2025 コミックつくば！準備会 All Rights Reserved.</p>
             </div>
         </footer>
+    );
+}
+
+function NavGroup({ title, links }: { title: string, links: { label: string, href: string }[] }) {
+    return (
+        <div className="flex flex-col gap-2 w-48">
+            <h2 className="font-bold">{title}</h2>
+            <ul className="flex flex-col gap-0">
+                {links.map(link => (
+                    <li key={link.label}>
+                        <Link to={link.href}>{link.label}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
