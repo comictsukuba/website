fc72f3e26d082acc11b6ba2e5469b54c3453ff5b
Zemelua <red0165333@gmail.com>
2025-09-17 11:42:59 +0900

    style: ヘッダーを改修

--- app/components/common/header/HamburgerMenu.tsx
+++ app/components/common/header/HamburgerMenu.tsx
@@ -1,48 +1,90 @@
-import { Menu, X } from "lucide-react";
+import { Menu, XIcon } from "lucide-react";
+import { motion } from "motion/react";
 import { useState, type JSX } from "react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export function HamburgerMenu(): JSX.Element {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-      {/* ハンバーガーメニューアイコン */}
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="text-white focus:outline-none"
-      >
-        {isOpen ? <X size={24} /> : <Menu size={24} />}
-      </button>
+	function handleClose() {
+		setIsOpen(false);
+	}
 
-      {/* オーバーレイ */}
-      {isOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40"
-          onClick={() => setIsOpen(false)}
-        />
-      )}
+	return (
+		<>
+			{/* ハンバーガーメニューアイコン */}
+			<button
+				onClick={() => setIsOpen(!isOpen)}
+				className="text-black h-full px-4 flex items-center justify-center">
+				<div className="absolute w-20 h-10 -rotate-10 rounded-[50%] bg-brand-main" />
+				<Menu size={24} className="z-0" />
+			</button>
 
-      {/* スライドメニュー */}
-      <div
-        className={`fixed top-0 right-0 w-64 h-full bg-[#00C1D4] shadow-md p-4 transform transition-transform duration-300 z-50 ${isOpen ? "translate-x-0" : "translate-x-full"
-          }`}
-      >
-        <ul className="flex flex-col space-y-8 text-white">
-          <li>
-            <Link to="/">- HOME</Link>
-          </li>
-          <li>
-            <Link to="/about">- コミックつくばとは？</Link>
-          </li>
-          <li>
-            <Link to="/application">- 参加案内</Link>
-          </li>
-          <li>
-            <Link to="/contact">- お問い合わせ</Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+			{/* オーバーレイ */}
+			{isOpen && (
+				<div
+					className="fixed inset-0 bg-black/10 bg-opacity-50 z-40"
+					onClick={() => setIsOpen(false)}
+				/>
+			)}
+
+			{/* スライドメニュー */}
+			<motion.div
+				className={"fixed top-0 right-0 w-[320px] h-full bg-white p-4 z-50 flex flex-col items-end gap-4"}
+				initial={{ x: "100%" }}
+				animate={{ x: isOpen ? 0 : "100%" }}
+				transition={{ duration: 0.15, type: "tween", ease: "easeOut" }}
+			>
+				<button
+					onClick={() => setIsOpen(!isOpen)}
+					className="text-black w-fit h-fit px-6 py-4 flex items-center justify-center">
+					<XIcon size={24} className="z-0" />
+				</button>
+				<nav className="w-full flex flex-col gap-0 text-black">
+					<ul className="contents">
+						<li className="h-fit">
+							<NavLinkItem to="/" handleClose={handleClose}>HOME</NavLinkItem>
+						</li>
+						<li className="h-fit">
+							<NavLinkItem to="/about" handleClose={handleClose}>コミックつくばとは？</NavLinkItem>
+						</li>
+						<li className="h-fit">
+							<NavLinkItem to="/application" handleClose={handleClose}>参加案内</NavLinkItem>
+						</li>
+						<li className="h-fit">
+							<NavLinkItem to="/contact" handleClose={handleClose}>お問い合わせ</NavLinkItem>
+						</li>
+					</ul>
+				</nav>
+			</motion.div>
+		</>
+	);
+}
+
+function NavLinkItem({ to, children, handleClose }: { to: string; children: React.ReactNode; handleClose: () => void }): JSX.Element {
+	return (
+		<NavLink to={to} className="w-full h-full flex items-center justify-center relative px-4 py-4" onClick={handleClose}>
+			{
+				({ isActive }) => (
+					<>
+						{
+							isActive && <motion.div
+								className="absolute bg-brand-main w-20 h-10 rounded-[50%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+								transition={{ type: "spring", duration: 0.5 }}
+								initial={{ rotate: -10 }}
+								animate={{ rotate: -10 }}
+								layoutId="hamburger-nav-link-active-indicator"
+							/>
+						}
+						<motion.span className={`z-10 whitespace-nowrap text-black`}
+							animate={{
+								fontWeight: isActive ? 700 : 500,
+							}}>
+							{children}
+						</motion.span>
+					</>
+				)
+			}
+		</NavLink>
+	);
 }
