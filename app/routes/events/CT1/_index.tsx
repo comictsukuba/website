a7201834e34fbaa31d18783f141cd67249555d3b
Zemelua <zemelua1107@gmail.com>
2025-10-29 12:27:05 +0900

    Update _index.tsx

--- app/routes/events/CT1/_index.tsx
+++ app/routes/events/CT1/_index.tsx
@@ -27,7 +27,7 @@ export default function Index({ loaderData }: Route.ComponentProps): JSX.Element
 				{/* メタデータ -----✧ */}
 				<title>第１回コミックつくば！開催案内 ✧ コミックつくば！</title>
 				<meta name="description" content="コミックつくば！公式サイトです。"></meta>
-				<meta property="og:image" content="/pictures/banner_CT1.png"></meta>
+				<meta property="og:image" content="https://comictsukuba.com/pictures/banner_CT1.png"></meta>
 				<meta property="og:url" content="https://comictsukuba.com/events/CT1"></meta>
 				<meta property="og:title" content="第１回コミックつくば！開催案内"></meta>
 				<meta name="twitter:card" content="summary"></meta>
